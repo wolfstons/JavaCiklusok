27591ab77ffc4f291169b77d833e76c7dcbe29f4
Páczi Balázs (SZF_N_2025_2027) <paczi.balazs@diak.szamalk-szalezi.hu>
2026-01-30 11:07:09 +0100

    ciklusok

---
+++ ciklusok.java
@@ -0,0 +1,39 @@
+package kiirasok;
+
+/**
+ *
+ * @author PácziBalázs(SZF_N_20
+ */
+public class ciklusok {
+
+    public static void main(String[] args) {
+        int SorDb = 5;
+        int SzamDb = 5;
+        String formazo = "%" + SzamDb + "s\n";
+        char Betu = 'a';
+        int BetuDb = 4;
+
+        String szoveg = " " + Betu;
+        int cv = 0;
+        while (cv < BetuDb - 1) {
+            szoveg += Betu;
+            cv++;
+        }
+        cv = 0;
+        if (BetuDb >= SorDb) {
+            int tmp= BetuDb;
+            BetuDb=SzamDb;
+            
+
+            while (cv < SorDb) {
+                System.out.printf(formazo, szoveg);
+                cv++;
+            }
+        } else {
+            while (cv < SorDb) {
+                System.out.printf(szoveg, formazo);
+                cv++;
+            }
+        }
+    }
+}
